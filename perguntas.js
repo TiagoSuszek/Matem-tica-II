72139d2c5d60b038a6eaab10e80b92a2d110b63d
TiagoSuszek <suszek.goncalves@escola.pr.gov.br>
2025-05-27 08:41:49 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Computador',
+    'Quando lançou o Windows xp?',
+    '25 de outubro de 2001'
+)
+
+criaCartao(
+    'Computador',
+    'Quando lançou o Windows 7?',
+    '22 de julho de 2009'
+)
+
+criaCartao(
+    'Computador',
+    'Quando lançou o Windows 8?',
+    '26 de outubro de 2012'
+)
